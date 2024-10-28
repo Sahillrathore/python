ca322886f835aeec60cbeb2208f9850a1bf09256
Sahillrathore <sahil921031@gmail.com>
2024-10-29 00:07:30 +0530

    reverse pat

--- first.py
+++ first.py
@@ -9,3 +9,16 @@ while num <= limit:
         num2 = num2 + 1
     print('')
     num = num + 1
+
+
+limitR = int(input("Enter Number : "))
+
+r = 1
+
+while r <= limitR:
+    num2 = limitR
+    while num2 >= r:
+        print(num2, end="")
+        num2 = num2 - 1
+    print('')
+    r = r + 1
