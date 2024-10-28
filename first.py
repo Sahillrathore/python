158680431a5cd40d94d1ee3c08772ef0787adcd9
Sahillrathore <sahil921031@gmail.com>
2024-10-28 23:56:29 +0530

    py cmt

--- first.py
+++ first.py
@@ -9,3 +9,1026 @@ while num <= limit:
         num2 = num2 + 1
     print('')
     num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
+limit = int(input("Enter Number: "))
+
+num = 1
+
+while num <= limit:
+    num2 = 1
+    while num2 <= num:
+        print(num2, end="")
+        num2 = num2 + 1
+    print('')
+    num = num + 1
