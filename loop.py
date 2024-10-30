5342dbc767f7bac44605009e2c7787f93f26d7de
Sahillrathore <sahil921031@gmail.com>
2024-10-30 23:14:36 +0530

    rev loop

--- loop.py
+++ loop.py
@@ -5,3 +5,11 @@ while n <= 10:
     print(num, "*", n, "=", num*n)
     print()
     n = n + 1
+
+num = int(input('Enter Number for Table: '))
+
+n = 10
+while n >= 1:
+    print(num, "*", n, "=", num*n)
+    print()
+    n = n - 1
