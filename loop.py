1ac8e74adc58d5ac11dbbcedb87882f49489e154
Sahillrathore <sahil921031@gmail.com>
2024-10-31 17:52:04 +0530

    nmn

--- loop.py
+++ loop.py
@@ -1,15 +1,15 @@
-# num = int(input('Enter Number for Table: '))
+num = int(input('Enter Number for Table: '))
 
-# n = 1
-# while n <= 10:
-#     print(num, "*", n, "=", num*n)
-#     print()
-#     n = n + 1
+n = 1
+while n <= 10:
+    print(num, "*", n, "=", num*n)
+    print()
+    n = n + 1
 
-# num = int(input('Enter Number for Table: '))
+num = int(input('Enter Number for Table: '))
 
-# n = 10
-# while n >= 1:
-#     print(num, "*", n, "=", num*n)
-#     print()
-#     n = n - 1
+n = 10
+while n >= 1:
+    print(num, "*", n, "=", num*n)
+    print()
+    n = n - 1
